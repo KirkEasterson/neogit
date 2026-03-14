66aba6eba188603f2e06ff3c5bb7199fbfcffa8f
Kirk Easterson <kirk.easterson@gmail.com>
2026-03-14 21:17:53 +0100

    Hook for `PreBisect`
    A hook for the "cmd" and "finished" bisect was omitted. It would have no
    difference compared to the event.

--- lua/neogit/config.lua
+++ lua/neogit/config.lua
@@ -344,6 +344,7 @@ end
 ---| "PreStash"
 ---| "PreRefsRefreshed"
 ---| "PreDiffLoaded"
+---| "PreBisect"
 
 ---@class NeogitConfigStatusOptions
 ---@field recent_commit_count? integer The number of recent commits to display
